9df4babcbd41a3c9254b95b3b62bc016ff2aa49b
Alvin Febriando <alvinfebriando@outlook.com>
2024-01-18 21:44:13 +0700

    feat: add guest handlers

--- handler/guest.js
+++ handler/guest.js
@@ -1,5 +1,76 @@
-export const addGuestHandler = (req, res) => {
-  res.status(201).json({
-    data: req.body,
+import { Guest } from '../repository/model.js';
+
+export const getAllGuestHandler = async (req, res) => {
+  let guests = await Guest.findAll();
+  res.status(200).json({
+    data: guests,
+  });
+};
+
+export const getGuestById = async (req, res) => {
+  let guest = await Guest.findByPk(req.params.id);
+  if (!guest) {
+    res.status(404);
+    res.json({
+      error: 'guest not found',
+    });
+    return;
+  }
+
+  res.json({
+    data: guest,
+  });
+};
+
+export const addGuestHandler = async (req, res) => {
+  try {
+    await Guest.create({
+      name: req.body.name,
+      address: req.body.address,
+      phoneNumber: req.body.phone_number,
+      notes: req.body.notes,
+    });
+  } catch (err) {
+    res.status(500);
+    res.json({
+      error: err,
+    });
+  }
+
+  res.status(201);
+  res.json({
+    message: 'saved',
+  });
+};
+
+export const updateGuest = async (req, res) => {
+  let guest = await Guest.findByPk(req.params.id);
+  if (!guest) {
+    res.status(400);
+    res.json({
+      message: 'guest not found',
+    });
+  }
+
+  guest.name = req.body.name;
+  guest.address = req.body.address;
+  guest.phoneNumber = req.body.phone_number;
+  guest.notes = req.body.notes;
+
+  await guest.save();
+
+  res.json({
+    message: 'updated',
+  });
+};
+
+export const deleteGuestHandler = async (req, res) => {
+  await Guest.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json({
+    message: 'deleted',
   });
 };
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import bodyparser from 'body-parser';
 import express from 'express';
-import { addGuestHandler } from './handler/guest.js';
+import {
+  addGuestHandler,
+  getAllGuestHandler,
+  getGuestById,
+  updateGuest,
+  deleteGuestHandler,
+} from './handler/guest.js';
 import { helloHandler } from './handler/hello.js';
 
 const app = express();
@@ -9,7 +15,12 @@ const port = 3000;
 app.use(bodyparser.json());
 
 app.get('/', helloHandler);
+
+app.get('/guests', getAllGuestHandler);
+app.get('/guests/:id', getGuestById);
+app.put('/guests/:id', updateGuest);
 app.post('/guests', addGuestHandler);
+app.delete('/guests/:id', deleteGuestHandler);
 
 app.listen(port, () => {
   console.log(`running on port: ${port}`);
